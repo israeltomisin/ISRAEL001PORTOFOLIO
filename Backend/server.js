fa93069323b84cc885a29fd305b338ff89b8486f
adekunled39 <israeltomisin001@yahoo.com>
2026-03-04 12:51:51 +0100

    backend

--- Backend/server.js
+++ Backend/server.js
@@ -7,9 +7,16 @@ import nodemailer from "nodemailer";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+// const PORT = 3000;
 
 // Middleware
+// app.use(cors());
+const cors = require("cors");
 app.use(cors());
 app.use(bodyParser.json());
 
